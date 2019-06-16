26686d37c4af353720dcabb28fcfbcdc7489199e
DavidFirth <davidf@veryspeedy.net>
2019-06-16 09:30:39 +0100

    Put the Q-Q plot in a vetter place

--- R/functions.R
+++ R/functions.R
@@ -65,19 +65,6 @@ check_modules_expected <- function(working_directory, module_codes){
     list(extras = extras, missing = missing)
 }
 
-qqplot.data <- function (vec) # argument: vector of numbers
-{
-  # following four lines from base R's qqline()
-  y <- quantile(vec[!is.na(vec)], c(0.25, 0.75))
-  x <- qnorm(c(0.25, 0.75))
-  slope <- diff(y)/diff(x)
-  int <- y[1L] - slope * x[1L]
-
-  d <- data.frame(resids = vec)
-
-  ggplot(d, aes(sample = resids)) + stat_qq() + geom_abline(slope = slope, intercept = int)
-
-}
 
 #' Print and return a table of module effects
 #'
